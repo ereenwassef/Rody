00313c141c4710bbea89571003f1d58af2a8852f
ereenwassef <ereenwassef@gmail.com>
2020-08-16 00:20:17 +0200

    Rody file

---
+++ rody_sys/Areas/adminPanel/Controllers/generalController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using rody_sys.Areas.adminPanel.Models;
+using System.Data.Entity;
+using System.Threading;
+using System.Globalization;
+using rody_sys.Models;
+
+namespace rody_sys.Areas.adminPanel.Controllers
+{
+    public class generalController : Controller
+    {
+
+       public Rody_DBEntities3 DB = new Rody_DBEntities3();
+
+ 
+
+	}
+}
